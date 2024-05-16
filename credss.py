8dba29d0ae2880d84190d3125df73665a57c1793
Nouh BOUHAJIB <bouhajib@gmail.com>
2024-05-16 19:34:13 +0200

    Delete credss.py

--- credss.py
+++
@@ -1,9 +0,0 @@
-KEY_ID = "your_key_id"
-SECRET_KEY = "your_secret_key"
-BASE_URL = "https://paper-api.alpaca.markets/v2"
-
-APLACA_CREDS = {
-    "API_KEY": KEY_ID,
-    "API_SECRET": SECRET_KEY,
-    "PAPER": True
-}
